0780a5575985d2523e61db604560976ee733a39a
markobyte <marko@zzzel.xyz>
2019-01-29 19:07:31 +0100

    [MAIN] Adds documentation for AES256 and EHREncryption

--- Krypt/Source/AES256.swift
+++ Krypt/Source/AES256.swift
@@ -10,16 +10,34 @@ import CommonCrypto
 import CryptoSwift
 import Foundation
 
+/// Symetric encryption used in Vivy.
+///
+/// Only 256 bits (32 bytes) long keys are supported along with
+/// GCM and CBC being the only supported block modes
 public struct AES256 {
+  /// Error returned when dealing with CommonCrypto or in this case with CBC block mode AES
+  ///
+  /// - ccError: with `CCCryptorStatus`
   public enum Error: LocalizedError {
     case ccError(status: CCCryptorStatus)
   }
 
+  /// Supported block modes
+  ///
+  /// - gcm: standart block mode for e2ee in Vivy
+  /// - cbc: legacy block mode only used for mainly for decreption
   public enum BlockMode {
     case gcm
     case cbc
   }
 
+  /// - Parameters:
+  ///   - data: data to encrypt
+  ///   - key: authentication key
+  ///   - iv: initialization vector
+  ///   - blockMode: which `BlockMode` to use
+  /// - Returns: tuple of encrypted data, authentication key and initialization vector used for encryption
+  /// - Throws: `ccError` or some CryptoSwift errors if using GCM
   public static func encrypt(data: Data, key: Data? = nil, iv: Data? = nil, blockMode: BlockMode) throws -> (encrypted: Data, key: Data, iv: Data) {
     switch blockMode {
     case .gcm:
@@ -29,6 +47,13 @@ public struct AES256 {
     }
   }
 
+  /// - Parameters:
+  ///   - data: data to decrypt
+  ///   - key: authentication key
+  ///   - iv: initialization vector
+  ///   - blockMode: which `BlockMode` to use
+  /// - Returns: decrypted data
+  /// - Throws: `ccError` or some CryptoSwift errors if using GCM
   public static func decrypt(data: Data, key: Data, iv: Data, blockMode: BlockMode) throws -> Data {
     switch blockMode {
     case .gcm:
@@ -41,6 +66,10 @@ public struct AES256 {
 }
 
 private extension AES256 {
+  /// Generates random data of provided length
+  ///
+  /// - Parameter count: length of data
+  /// - Returns: random data
   static func randomData(count: Int) -> Data {
     var data = Data(count: count)
     let status = data.withUnsafeMutableBytes {
@@ -52,9 +81,15 @@ private extension AES256 {
     return data
   }
 
+  /// Encrypts data with GCM block mode
+  /// Key and IV are randomly generated inside
+  ///
+  /// - Parameter data: data to encrypt
+  /// - Returns: tuple of encrypted data, authentication key and initialization vector
+  /// - Throws: any errors throws by CryptoSwift
   static func encryptGCM(data: Data) throws -> (encrypted: Data, key: Data, iv: Data) {
-    let key = AES256.randomData(count: kCCKeySizeAES256)
-    let iv = AES256.randomData(count: kCCKeySizeAES128)
+    let key = randomData(count: kCCKeySizeAES256)
+    let iv = randomData(count: kCCKeySizeAES128)
 
     let aes = try AES(key: key.bytes, blockMode: GCM(iv: iv.bytes, mode: .combined), padding: .noPadding)
     let digest = try aes.encrypt(data.bytes)
@@ -63,6 +98,14 @@ private extension AES256 {
     return (encrypted, key, iv)
   }
 
+  /// Decrypts data with GCM block mode
+  ///
+  /// - Parameters:
+  ///   - data: data to decrypt
+  ///   - key: authentication key
+  ///   - iv: initialization vector
+  /// - Returns: decrypted data
+  /// - Throws: any errors throws by CryptoSwift
   static func decryptGCM(data: Data, key: Data, iv: Data) throws -> Data {
     let aes = try AES(key: key.bytes, blockMode: GCM(iv: iv.bytes, mode: .combined), padding: .noPadding)
     let digest = try aes.decrypt(data.bytes)
@@ -71,6 +114,16 @@ private extension AES256 {
     return decrypted
   }
 
+  /// Single function for encryption with CBC block mode
+  /// If key and/or IV are not provided, this functions generates a secure random data of correct length
+  ///
+  /// - Parameters:
+  ///   - data: data to encrypt/decrypt
+  ///   - key: authentication key
+  ///   - iv: initialization vector
+  ///   - operation: kCCEncrypt/kCCDecrypt
+  /// - Returns: tuple of encrypted data, authentication key and initialization vector
+  /// - Throws: any errors returned by CommonCrypto
   static func cryptCBCPKCS7(
     data: Data,
     key: Data?,
@@ -79,8 +132,8 @@ private extension AES256 {
   ) throws -> (encrypted: Data, key: Data, iv: Data) {
     let algorithm = CCAlgorithm(kCCAlgorithmAES)
     let options = CCOptions(kCCOptionPKCS7Padding)
-    let key = key ?? AES256.randomData(count: kCCKeySizeAES256)
-    let iv = iv ?? AES256.randomData(count: kCCKeySizeAES128)
+    let key = key ?? randomData(count: kCCKeySizeAES256)
+    let iv = iv ?? randomData(count: kCCKeySizeAES128)
     var dataOut = [UInt8](repeating: 0, count: data.count + kCCBlockSizeAES128)
     var dataOutMoved = 0
     var status: CCCryptorStatus!
