9dca143499e67a5bee6e0ec584fe6ea73d935afc
markobyte <marko@zzzel.xyz>
2019-01-30 11:18:50 +0100

    RSA, Key

--- Krypt/Source/Key.swift
+++ Krypt/Source/Key.swift
@@ -8,11 +8,20 @@
 import Foundation
 import Security
 
+/// High level object representing an RSA key to be used for asymetric encryption.
+/// Currently only RSA keys with 4096 bits length are supported.
 public final class Key {
+  /// Error object containing erros that might occur during converting keys to different formats
+  ///
+  /// - invalidPEMData: data was not in PKCS#1 (for private) or PKCS#8 (for public) format when initializing `Key` from PEM data
   public enum Error: LocalizedError {
     case invalidPEMData
   }
 
+  /// Access level of the key
+  ///
+  /// - `public`: for public key
+  /// - `private`: for private key
   public enum Access {
     case `public`
     case `private`
@@ -27,6 +36,12 @@ public final class Key {
 }
 
 public extension Key {
+  /// Initializes `Key` from DER data
+  ///
+  /// - Parameters:
+  ///   - der: `Data` in DER format
+  ///   - access: `Access` level of the key
+  /// - Throws: any erros that are returned by `SecKeyCreateWithData`
   public convenience init(der: Data, access: Access) throws {
     let options: [String: Any] = [
       kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
@@ -40,6 +55,12 @@ public extension Key {
     self.init(key: key, access: access)
   }
 
+  /// Initializes `Key` from PEM data
+  ///
+  /// - Parameters:
+  ///   - pem: `Data` in PKCS#8 format for public | PKCS#1 for private access
+  ///   - access: `Access` level of the key
+  /// - Throws: any errors that can occur while converting the key from PEM -> DER -> SecKey
   public convenience init(pem: Data, access: Access) throws {
     guard let pemString = String(data: pem, encoding: .utf8) else {
       throw Error.invalidPEMData
@@ -54,6 +75,10 @@ public extension Key {
     try self.init(der: der, access: access)
   }
 
+  /// Converts the underlying `secRef` to PKCS#1 DER format for public and private access
+  ///
+  /// - Returns: `Data` representation of the key in DER format
+  /// - Throws: errors occuring during `SecKeyCopyExternalRepresentation`
   public func convertedToDER() throws -> Data {
     var error: Unmanaged<CFError>?
     guard let der = SecKeyCopyExternalRepresentation(secRef, &error) as Data? else {
@@ -64,6 +89,7 @@ public extension Key {
 }
 
 private extension Key.Access {
+  /// Security attribute for the key class depending on the access
   var secAttr: CFString {
     switch self {
     case .public:
@@ -73,6 +99,8 @@ private extension Key.Access {
     }
   }
 
+  /// PEM header used when importing keys.
+  /// Currently supports only PKCS#8 format for public and PKCS#1 for private
   var pemHeader: String {
     switch self {
     case .public:
@@ -82,6 +110,8 @@ private extension Key.Access {
     }
   }
 
+  /// PEM footer used when importing keys.
+  /// Currently supports only PKCS#8 format for public and PKCS#1 for private
   var pemFooter: String {
     switch self {
     case .public:
