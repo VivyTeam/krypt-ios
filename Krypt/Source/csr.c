f5316820bfa08f8c7cc9c643f32af7de4b06049d
markobyte <marko@zzzel.xyz>
2019-03-13 16:38:25 +0100

    replace goto with function

--- Krypt/Source/csr.c
+++ Krypt/Source/csr.c
@@ -12,12 +12,8 @@
 #include <openssl/pem.h>
 #include <openssl/x509.h>
 
-EVP_PKEY *getPrivateKey(const char *key) {
-  BIO *membuf = BIO_new(BIO_s_mem());
-  BIO_puts(membuf, key);
-  EVP_PKEY *privateKey = PEM_read_bio_PrivateKey(membuf, NULL, 0, NULL);
-  return privateKey;
-}
+EVP_PKEY *getPrivateKey(const char *key);
+void freeAll(X509_REQ *req, BIO *out, EVP_PKEY *key);
 
 char *createCSR(const char *key, const char *country, const char *state, const char *location, const char *organization, const char *organizationUnit, const char *emailAddress) {
 
@@ -35,7 +31,8 @@ char *createCSR(const char *key, const char *country, const char *state, const c
   x509_req = X509_REQ_new();
   ret = X509_REQ_set_version(x509_req, version);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   // set subject of x509 req
@@ -43,37 +40,44 @@ char *createCSR(const char *key, const char *country, const char *state, const c
 
   ret = X509_NAME_add_entry_by_txt(x509_name, "C", MBSTRING_ASC, (const unsigned char*)country, -1, -1, 0);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   ret = X509_NAME_add_entry_by_txt(x509_name, "ST", MBSTRING_ASC, (const unsigned char*)state, -1, -1, 0);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   ret = X509_NAME_add_entry_by_txt(x509_name, "L", MBSTRING_ASC, (const unsigned char*)location, -1, -1, 0);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   ret = X509_NAME_add_entry_by_txt(x509_name, "O", MBSTRING_ASC, (const unsigned char*)organization, -1, -1, 0);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   ret = X509_NAME_add_entry_by_txt(x509_name, "OU", MBSTRING_ASC, (const unsigned char*)organizationUnit, -1, -1, 0);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   ret = X509_NAME_add_entry_by_txt(x509_name, "emailAddress", MBSTRING_ASC, (const unsigned char*)emailAddress, -1, -1, 0);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   ret = X509_REQ_set_subject_name(x509_req, x509_name);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   // set public key of x509 req
@@ -81,20 +85,23 @@ char *createCSR(const char *key, const char *country, const char *state, const c
 
   ret = X509_REQ_set_pubkey(x509_req, privateKey);
   if (ret != 1) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   // set sign key of x509 req
   ret = X509_REQ_sign(x509_req, privateKey, EVP_sha256());    // return x509_req->signature->length
   if (ret <= 0) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   // Convert to PEM
   out = BIO_new(BIO_s_mem());
   ret = PEM_write_bio_X509_REQ(out, x509_req);
   if (ret <= 0) {
-    goto free_all;
+    freeAll(x509_req, out, privateKey);
+    return NULL;
   }
 
   BUF_MEM* mem;
@@ -103,10 +110,18 @@ char *createCSR(const char *key, const char *country, const char *state, const c
   memcpy(data, mem->data, mem->length + 1);
   BIO_flush(out);
 
-free_all:
-  X509_REQ_free(x509_req);
-  BIO_free_all(out);
-  EVP_PKEY_free(privateKey);
-
   return data;
+}
+
+void freeAll(X509_REQ *req, BIO *out, EVP_PKEY *key) {
+  X509_REQ_free(req);
+  BIO_free_all(out);
+  EVP_PKEY_free(key);
+}
+
+EVP_PKEY *getPrivateKey(const char *key) {
+  BIO *membuf = BIO_new(BIO_s_mem());
+  BIO_puts(membuf, key);
+  EVP_PKEY *privateKey = PEM_read_bio_PrivateKey(membuf, NULL, 0, NULL);
+  return privateKey;
 }
