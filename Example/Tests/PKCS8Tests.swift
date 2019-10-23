98ddb16f65d87f398d9f4fbcd3f9b07df4e11ce6
marko <marko@hungry.dev>
2019-10-23 14:29:00 +0200

    Adds encryption parameters test

--- Example/Tests/PKCS8Tests.swift
+++ Example/Tests/PKCS8Tests.swift
@@ -94,4 +94,61 @@ final class PKCS8Tests: XCTestCase {
     // then
     XCTAssertNotNil(encrypted)
   }
+
+  /// parameters were extracted using this online tool: https://lapo.it/asn1js/
+  func testEncryptPKCS1_simplePassword__shouldContainRightEncryptionParameters() {
+    // given
+    let pkcs1PEMData = TestData.openSSLPrivateKeyPEM.data
+    let password = "password"
+
+    let expectedPBEAlgorithmBytes: [UInt8] = [0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x05, 0x0D] // 1.2.840.113549.1.5.13 pkcs5PBES2 (PKCS #5 v2.0)
+    let expectedKDFBytes: [UInt8] = [0x06, 0x09, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x05, 0x0C] // 1.2.840.113549.1.5.12 pkcs5PBKDF2 (PKCS #5 v2.0)
+    let expectedKDFIterationCountBytes: [UInt8] = [0x02, 0x03, 0x01, 0x86, 0xA0] // 100_000 iterations for PBKDF2
+    let expectedHashingAlgorithmBytes: [UInt8] = [0x06, 0x08, 0x2A, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x02, 0x09] // 1.2.840.113549.2.9 hmacWithSHA256 (RSADSI digestAlgorithm)
+    let expectedSymmetricEncryptionAlgorithmBytes: [UInt8] = [0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x01, 0x2A] // 2.16.840.1.101.3.4.1.42 aes256-CBC (NIST Algorithm)
+
+    // when
+    let encrypted = PKCS8.encrypt(pkcs1PEMData, password: password)
+
+    // then
+    XCTAssertNotNil(encrypted)
+
+    let der = encrypted!.der
+    XCTAssertEqual(der.passwordBasedEncryptionAlgorithmBytes, expectedPBEAlgorithmBytes)
+    XCTAssertEqual(der.keyDerivationFunctionBytes, expectedKDFBytes)
+    XCTAssertEqual(der.kdfIterationCountBytes, expectedKDFIterationCountBytes)
+    XCTAssertEqual(der.hashingAlgorithmBytes, expectedHashingAlgorithmBytes)
+    XCTAssertEqual(der.symmetricEncryptionAlgorithmBytes, expectedSymmetricEncryptionAlgorithmBytes)
+  }
+}
+
+private extension String {
+  var der: Data {
+    let stripped = self.replacingOccurrences(of: "-----BEGIN ENCRYPTED PRIVATE KEY-----", with: "")
+      .replacingOccurrences(of: "-----END ENCRYPTED PRIVATE KEY-----", with: "")
+      .replacingOccurrences(of: "\n", with: "")
+    return Data(base64Encoded: stripped)!
+  }
+}
+
+private extension Data {
+  var passwordBasedEncryptionAlgorithmBytes: [UInt8] {
+    return [UInt8](self[6...16])
+  }
+
+  var keyDerivationFunctionBytes: [UInt8] {
+    return [UInt8](self[21...31])
+  }
+
+  var kdfIterationCountBytes: [UInt8] {
+    return [UInt8](self[44...48])
+  }
+
+  var hashingAlgorithmBytes: [UInt8] {
+    return [UInt8](self[51...60])
+  }
+
+  var symmetricEncryptionAlgorithmBytes: [UInt8] {
+    return [UInt8](self[65...75])
+  }
 }
