e0fe580a5737f65671a7ce86e6063fd7cb27b885
Gilad Reich <17655494+giladreich@users.noreply.github.com>
2022-08-17 13:57:18 +0200

    [MAIN] Add tests from previous example project.

---
+++ Example/KryptExampleTests/KeyTests.swift
@@ -0,0 +1,133 @@
+//
+//  KeyTests.swift
+//  Krypt_Tests
+//
+//  Created by marko on 24.01.19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Krypt
+import XCTest
+
+final class KeyTests: XCTestCase {
+  func testInit_pemData_publicPEMFromOpenSSL__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPublicKeyPEM.data
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .public))
+  }
+
+  func testInit_pemString_publicPEMFromOpenSSL__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPublicKeyPEM.stringTrimmingWhitespacesAndNewlines
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .public))
+  }
+
+  func testInit_pemData_privatePEMFromOpenSSL__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPrivateKeyPEM.data
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .private))
+  }
+
+  func testInit_pemString_privatePEMFromOpenSSL__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPrivateKeyPEM.stringTrimmingWhitespacesAndNewlines
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .private))
+  }
+
+  func testConvertedToPEM_privatePEMFromOpenSSL__shouldMatchTestPEM() throws {
+    // given
+    let testPEM = TestData.openSSLPrivateKeyPEM.string
+
+    // when
+    let key = try Key(pem: testPEM, access: .private)
+
+    // then
+    let pem = try key.convertedToPEM()
+    XCTAssertEqual(pem, testPEM)
+  }
+
+  func testConvertedToPEM_publicPEMFromOpenSSL__shouldMatchTestPEM() throws {
+    // given
+    let testPEMData = TestData.openSSLPublicKeyPEM.data
+    let testPEM = String(data: testPEMData, encoding: .utf8)!
+
+    // when
+    let key = try Key(pem: testPEMData, access: .public)
+
+    // then
+    let pem = try key.convertedToPEM()
+    XCTAssertEqual(pem, testPEM)
+  }
+
+  func testInit_pemData_privateKey2048__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPrivateKey2048PEM.data
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .private, size: .bit_2048))
+  }
+
+  func testInit_pemString_privateKey2048__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPrivateKey2048PEM.stringTrimmingWhitespacesAndNewlines
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .private, size: .bit_2048))
+  }
+
+  func testInit_pemData_publicKey2048__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPublicKey2048PEM.data
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .public, size: .bit_2048))
+  }
+
+  func testInit_pemString_publicKey2048__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPublicKey2048PEM.stringTrimmingWhitespacesAndNewlines
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, access: .public, size: .bit_2048))
+  }
+
+  func testInit_pemString_publicKeyECSECPrime256R1PKCS8__shouldInitialize() throws {
+    // given
+    let pem = TestData.openSSLPublicKeyECPRIME256R1PKCS8PEM.stringTrimmingWhitespacesAndNewlines
+
+    // then
+    XCTAssertNoThrow(try Key(pem: pem, type: .ecSECPrimeRandom, access: .public, size: .bit_256))
+  }
+
+  func testDerivePublicKeyFromPrivateKey_usingPrivateKey_shouldReturnCorrectPublicKey() {
+    // given
+    let pem = TestData.openSSLPrivateKeyPEM.data
+    let privateKey = try! Key(pem: pem, access: .private)
+
+    // when
+    let publicKey = try? privateKey.publicKeyRepresentation()
+    let publicKeyPEM = try? publicKey?.convertedToPEM()
+
+    // then
+    XCTAssertEqual(publicKeyPEM, TestData.openSSLPublicKeyPEM.string)
+  }
+
+  func testDerivePublicKeyFromPrivateKey_usingPublicKey_shouldReturnErrorInvalidAccess() {
+    // given
+    let pem = TestData.openSSLPublicKeyPEM.data
+    let publicKey = try! Key(pem: pem, access: .public)
+
+    // then
+    XCTAssertThrowsError(try publicKey.publicKeyRepresentation(), "Should throw KeyError.invalidAccess error") { error in
+      XCTAssertEqual(error as? KeyError, KeyError.invalidAccess)
+    }
+  }
+}
---
+++ Example/KryptExampleTests/PEMConverterTests.swift
@@ -0,0 +1,134 @@
+//
+//  PEMConverterTests.swift
+//  Krypt_Tests
+//
+//  Created by marko on 08.05.19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+@testable import Krypt
+import XCTest
+
+final class PEMConverterTests: XCTestCase {
+  func testConvertToDER_pkcs1PrivateKey__shouldGiveValidDERData() {
+    // given
+    let testData = TestData.openSSLPrivateKeyPEM.string
+
+    // when
+    let data = PEMConverter.convertPEMToDER(testData)
+
+    // then
+    let attributes: [String: Any] = [
+      kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+      kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+      kSecAttrKeySizeInBits as String: 4096
+    ]
+    XCTAssertNotNil(SecKeyCreateWithData(data! as CFData, attributes as CFDictionary, nil))
+  }
+
+  func testConvertToDER_pkcs1PublicKey__shouldGiveValidDERData() {
+    // given
+    let testData = TestData.openSSLPublicKeyPKCS1PEM.string
+
+    // when
+    let data = PEMConverter.convertPEMToDER(testData)
+
+    // then
+    let attributes: [String: Any] = [
+      kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+      kSecAttrKeyClass as String: kSecAttrKeyClassPublic,
+      kSecAttrKeySizeInBits as String: 4096
+    ]
+    XCTAssertNotNil(SecKeyCreateWithData(data! as CFData, attributes as CFDictionary, nil))
+  }
+
+  func testConvertToDER_pkcs8PublicKey__shouldGiveValidDERData() {
+    // given
+    let testData = TestData.openSSLPublicKeyPEM.string
+
+    // when
+    let data = PEMConverter.convertPEMToDER(testData)
+
+    // then
+    let attributes: [String: Any] = [
+      kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+      kSecAttrKeyClass as String: kSecAttrKeyClassPublic,
+      kSecAttrKeySizeInBits as String: 4096
+    ]
+    XCTAssertNotNil(SecKeyCreateWithData(data! as CFData, attributes as CFDictionary, nil))
+  }
+
+  func testConvertToDER_certificate__shouldGiveValidDERData() {
+    // given
+    let testData = TestData.openSSLCertificateX509PEM.string
+
+    // when
+    let data = PEMConverter.convertPEMToDER(testData)
+
+    // then
+    XCTAssertNotNil(SecCertificateCreateWithData(nil, data! as CFData))
+  }
+
+  func testConvertDERToPEM_privateKey__shouldGiveExpectedPEM() {
+    // given
+    let expectedPEM = TestData.openSSLPrivateKeyPEM.stringTrimmingWhitespacesAndNewlines
+    let der = PEMConverter.convertPEMToDER(expectedPEM)!
+
+    // when
+    let pem = try? PEMConverter.convertDER(der, toPEMFormat: PEMFormat(contentType: .rsa, standard: .pkcs1, keyAccess: .private))
+
+    // then
+    XCTAssertEqual(pem?.trimmingCharacters(in: .whitespacesAndNewlines), expectedPEM)
+  }
+
+  func testConvertDERToPEM_publicKey__shouldGiveExpectedPKCS1PEM() {
+    // given
+    let expectedPEM = TestData.openSSLPublicKeyPKCS1PEM.string
+    let der = PEMConverter.convertPEMToDER(expectedPEM)!
+
+    // when
+    let pem = try? PEMConverter.convertDER(der, toPEMFormat: PEMFormat(contentType: .rsa, standard: .pkcs1, keyAccess: .public))
+
+    // then
+    XCTAssertEqual(pem, expectedPEM)
+  }
+
+  func testConvertDERToPEM_publicKey__shouldGiveExpectedPKCS8PEM() {
+    // given
+    let expectedPEM = TestData.openSSLPublicKeyPEM.string
+    let pkcs1PEM = TestData.openSSLPublicKeyPKCS1PEM.string
+    let der = PEMConverter.convertPEMToDER(pkcs1PEM)!
+
+    // when
+    let pem = try? PEMConverter.convertDER(der, toPEMFormat: PEMFormat(contentType: .rsa, standard: .pkcs8, keyAccess: .public))
+
+    // then
+    XCTAssertEqual(pem, expectedPEM)
+  }
+
+  func testConvertDERToPEM_certificate__shouldGiveExpectedPEM() {
+    // given
+    let expectedPEM = TestData.openSSLCertificateX509PEM.string
+    let der = PEMConverter.convertPEMToDER(expectedPEM)!
+
+    // when
+    let pem = try? PEMConverter.convertDER(der, toPEMFormat: PEMFormat(contentType: .x509, standard: .pkcs12, keyAccess: nil))
+
+    // then
+    XCTAssertEqual(pem, expectedPEM)
+  }
+
+  func testConvertDERToPEM_whenDERIsECFromSecKey__shouldCreatedExpectedPEM() {
+    // given
+    let secKeyDER = "BHB8S099Mg9Gp/NNJJn+8PEQ4smH5Tah09coYaxD5Te6Hi1gZAWxCmX998DkCqrolA8xpiJ8YXSW63uKG2ZlW7g="
+    let derData = Data(base64Encoded: secKeyDER)!
+    let format = PEMFormat(contentType: .ec, standard: .pkcs8, keyAccess: .public)
+    let expectedPEM = TestData.openSSLPublicKeyECPRIME256R1PKCS8PEM.string
+
+    // when
+    let pem = try? PEMConverter.convertDER(derData, toPEMFormat: format)
+
+    // then
+    XCTAssertEqual(pem, expectedPEM)
+  }
+}
